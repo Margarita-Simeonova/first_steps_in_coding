697c428ca2aefc13715a4c6a08d9fbb0e66b2c83
Margarita-Simeonova <margito.sl@gmail.com>
2024-10-11 18:40:46 +0300

    Add two new task.
    project_creation and pet_shop

---
+++ 07. Projects Creation.py
@@ -0,0 +1,13 @@
+def project_creation(name: str, projects_count: int):
+    ONE_PROJECT_TIME = 3
+    total_time = projects_count * ONE_PROJECT_TIME
+    result = (f"The architect {name} will need "
+              f"{total_time} hours to complete {projects_count}"
+              f" project/s.")
+    return result
+
+
+name_arg = input()
+projects_count_arg = int(input())
+result = project_creation(name_arg, projects_count_arg)
+print(result)
