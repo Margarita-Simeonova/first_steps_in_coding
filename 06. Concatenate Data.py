f44b5e8a460779095f9ca9f5862f1c7c91cb8289
Margarita-Simeonova <margito.sl@gmail.com>
2024-10-11 18:13:40 +0300

    concatenate_data
    Greeting by first name, last name, age and town.

---
+++ 06. Concatenate Data.py
@@ -0,0 +1,11 @@
+def concatenate_data(first_name: str, last_name: str, age: int, town: str):
+    greeting = f"You are {first_name} {last_name}, a {age}-years old person from {town}."
+    return greeting
+
+
+first_name_arg = input()
+last_name_arg = input()
+age_arg = int(input())
+town_arg = input()
+result = concatenate_data(first_name_arg, last_name_arg, age_arg, town_arg)
+print(result)
