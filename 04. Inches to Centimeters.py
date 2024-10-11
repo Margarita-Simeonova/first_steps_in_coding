54565ba435b98a5cac60e722a7f50400e83012bb
Margarita-Simeonova <margito.sl@gmail.com>
2024-10-11 17:34:10 +0300

    convertor
    converts inches to
    centimeters.

---
+++ 04. Inches to Centimeters.py
@@ -0,0 +1,8 @@
+def convertor(inches_value):
+    centimeters = inches_value * 2.54
+    return centimeters
+
+
+inches_value_arg = float(input())
+result = convertor(inches_value_arg)
+print(result)
