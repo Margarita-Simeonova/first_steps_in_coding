282426d39ffa8f26e267e7398d987bf2974151a4
Margarita-Simeonova <margito.sl@gmail.com>
2024-10-11 21:12:11 +0300

    money convertor
    money convertor

---
+++ 01. USD to BGN.py
@@ -0,0 +1,11 @@
+ONE_DOLLAR_PRICE = 1.79549
+
+
+def usd_to_bgn(dollars):
+    bgn = dollars * ONE_DOLLAR_PRICE
+
+    return bgn
+
+
+dollars_count = float(input())
+print(usd_to_bgn(dollars_count))
