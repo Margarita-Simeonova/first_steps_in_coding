b9846f2ddb1eb8c25e905935fa60a0056ee20dc4
Margarita-Simeonova <margito.sl@gmail.com>
2024-10-15 10:02:34 +0300

    Vacation books list
    Vacation books list

---
+++ 04. Vacation books list.py
@@ -0,0 +1,10 @@
+def vacation_books_list(pages_in_book, pages_per_hour, days):
+    days_needed = int((pages_in_book / pages_per_hour) / days)
+
+    return days_needed
+
+
+book_pages = int(input())
+pages = int(input())
+days = int(input())
+print(vacation_books_list(book_pages, pages, days))
