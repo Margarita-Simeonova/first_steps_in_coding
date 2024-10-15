e86fcf99a17ea2e5914ee53d59480394f7b0392d
Margarita-Simeonova <margito.sl@gmail.com>
2024-10-15 10:15:35 +0300

    Basketball Equipment
    Basketball Equipment

---
+++ 08. Basketball Equipment.py
@@ -0,0 +1,13 @@
+def basketball_equipment(subscription):
+    sneakers_price = subscription * 0.6
+    equipment_price = sneakers_price - (sneakers_price * 0.2)
+    ball_price = equipment_price / 4
+    accessories_price = ball_price / 5
+
+    total_expenses = subscription + sneakers_price + equipment_price + ball_price + accessories_price
+
+    return total_expenses
+
+
+tax = float(input())
+print(basketball_equipment(tax))
