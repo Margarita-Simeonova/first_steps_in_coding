b363c3b686839903bc37983aa5976bba41b73a5e
Margarita Simeonoff <90966716+Margarita-Simeonova@users.noreply.github.com>
2024-11-02 21:46:06 +0200

    Update 03. Deposit Calculator.py

--- 03. Deposit Calculator.py
+++ 03. Deposit Calculator.py
@@ -1,4 +1,5 @@
 def deposit_calculator(deposit_sum, period_in_mounts, interest_rate):
+    
     total_sum = deposit_sum + period_in_mounts * ((deposit_sum * interest_rate / 100) / 12)
 
     return total_sum
