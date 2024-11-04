9afa73aa0db7b869a500f05a692f30a1b826fcd7
Margarita Simeonoff <90966716+Margarita-Simeonova@users.noreply.github.com>
2024-11-04 11:17:51 +0200

    Update 05. Supplies for School.py

--- 05. Supplies for School.py
+++ 05. Supplies for School.py
@@ -4,6 +4,7 @@ DETERGENT = 1.2
 
 
 def supplies_for_school(pens_pack_count, markers_pack_count, detergent_litres, discount_percent):
+    
     price = pens_pack_count * PENS_PACK_PRICE + markers_pack_count * MARKERS_PACK_PRICE + detergent_litres * DETERGENT
     total_price = price - (price * (discount_percent / 100))
 
@@ -14,4 +15,6 @@ pens = int(input())
 markers = int(input())
 detergent = int(input())
 percent = float(input())
-print(supplies_for_school(pens, markers, detergent, percent))
+result = supplies_for_school(pens, markers, detergent, percent)
+
+print(result)
