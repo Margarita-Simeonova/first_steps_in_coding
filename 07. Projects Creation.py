6237ec6c5fc1d9d97a2cf0f6c42e9f65a30973be
Margarita Simeonoff <90966716+Margarita-Simeonova@users.noreply.github.com>
2024-11-05 08:08:13 +0200

    Update 07. Projects Creation.py

--- 07. Projects Creation.py
+++ 07. Projects Creation.py
@@ -13,4 +13,5 @@ def project_creation(name: str, projects_count: int):
 name_arg = input()
 projects_count_arg = int(input())
 result = project_creation(name_arg, projects_count_arg)
+
 print(result)
