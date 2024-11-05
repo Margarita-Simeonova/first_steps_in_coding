eb9906de784a5ef727904788f2d3e94379efc4ae
Margarita Simeonoff <90966716+Margarita-Simeonova@users.noreply.github.com>
2024-11-05 10:36:09 +0200

    Update 09. Yard Greening.py

--- 09. Yard Greening.py
+++ 09. Yard Greening.py
@@ -1,11 +1,14 @@
 def yard_greening(yard_area_in_square_meters):
+    
     price_without_discount = yard_area_in_square_meters * 7.61
     discount = price_without_discount * 0.18
     total_price = price_without_discount - discount
     result = f"The final price is: {total_price:.2f} lv.\nThe discount is: {discount:.2f} lv."
+    
     return result
 
 
 yard_area_in_square_meters_arg = float(input())
 result = yard_greening(yard_area_in_square_meters_arg)
+
 print(result)
