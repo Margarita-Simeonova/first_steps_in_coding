e5a4d00c428155811a2cbfe0779263c136edd132
Margarita Simeonoff <90966716+Margarita-Simeonova@users.noreply.github.com>
2024-11-12 13:20:53 +0200

    Update 02. numbers_line.py

--- 02. numbers_line.py
+++ 02. numbers_line.py
@@ -1,6 +1,6 @@
 #first option
 def numbers_line():
-    
+    # numbers from 1 to 10
     for n in range(1, 11):
         print(n)
 
@@ -10,10 +10,11 @@ numbers_line()
 #############
 #second opption
 def nums_line(start_num):
-    
+    # numbers from numb_x to numb_x + 10
     for n in range(start_num, start_num + 10):
         print(n)
 
 
+# return line of numbers
 number = 1
 nums_line(number)
