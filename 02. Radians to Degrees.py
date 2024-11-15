7872dc54f0b54c09f20c6d014e0f213c3445bd84
Margarita Simeonoff <90966716+Margarita-Simeonova@users.noreply.github.com>
2024-11-15 06:52:38 +0200

    Update 02. Radians to Degrees.py

--- 02. Radians to Degrees.py
+++ 02. Radians to Degrees.py
@@ -2,16 +2,16 @@ import math
 
 
 def radians_to_degrees(rad):
-  #calculate radians to degrees
+  # calculate radians to degrees
   
     degrees = rad * 180 / math.pi
 
     return degrees
 
 
-#get input
+# get input
 radians = float(input())
 result = radians_to_degrees(radians)
 
-#print result
+# print result
 print(result)
