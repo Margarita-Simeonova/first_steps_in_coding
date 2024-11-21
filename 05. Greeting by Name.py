c5d4e482c25f785046193a11a1a8bf695337b774
Margarita Simeonoff <90966716+Margarita-Simeonova@users.noreply.github.com>
2024-11-21 17:43:48 +0200

    Update 05. Greeting by Name.py

--- 05. Greeting by Name.py
+++ 05. Greeting by Name.py
@@ -1,10 +1,15 @@
 def greeting_by_name(name):
+    # define the function
     
     greeting = f"Hello, {name}!"
+    
+    # return the resulr
     return greeting
 
 
+# get the input from console
 name_arg = input()
 result = greeting_by_name(name_arg)
 
+# print result
 print(result)
