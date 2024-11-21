ab7fb7fe0c9db3ea0f42778ffbf2fe37a8674775
Margarita Simeonoff <90966716+Margarita-Simeonova@users.noreply.github.com>
2024-11-21 18:08:28 +0200

    Update 06. Concatenate Data.py

--- 06. Concatenate Data.py
+++ 06. Concatenate Data.py
@@ -1,6 +1,8 @@
 def concatenate_data(first_name: str, last_name: str, age: int, town: str):
     
     greeting = f"You are {first_name} {last_name}, a {age}-years old person from {town}."
+    
+    # return result
     return greeting
 
 
