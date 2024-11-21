101f9cf0d3ef7ebcff82245ab2b802661cbfce87
Margarita Simeonoff <90966716+Margarita-Simeonova@users.noreply.github.com>
2024-11-21 17:50:45 +0200

    Update 05. Supplies for School.py

--- 05. Supplies for School.py
+++ 05. Supplies for School.py
@@ -1,13 +1,16 @@
+# define the global variable 
 PENS_PACK_PRICE = 5.8
 MARKERS_PACK_PRICE = 7.2
 DETERGENT = 1.2
 
 
 def supplies_for_school(pens_pack_count, markers_pack_count, detergent_litres, discount_percent):
+    # define the function
     
     price = pens_pack_count * PENS_PACK_PRICE + markers_pack_count * MARKERS_PACK_PRICE + detergent_litres * DETERGENT
     total_price = price - (price * (discount_percent / 100))
 
+    # return result
     return total_price
 
 
